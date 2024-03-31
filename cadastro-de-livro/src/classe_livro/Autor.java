a0df7394ab944d13c49788a8b81b7befaf828d9d
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-03-31 13:00:56 -0300

    criação dos atributos e do método da classe autor

--- cadastro-de-livro/src/classe_livro/Autor.java
+++ cadastro-de-livro/src/classe_livro/Autor.java
@@ -1,5 +1,14 @@
 package classe_livro;
 
 public class Autor {
-    
+    String nome;
+    String email;
+    String cpf;
+
+    void mostrarDetalhes() {
+        System.out.println("Detalhes do autor: ");
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+        System.out.println("CPF: " + cpf);
+    }
 }
